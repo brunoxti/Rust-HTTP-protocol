b5a1aaa159e8de98db13a91a26557e06c61868f8
Bruno Alves <bruno.xti@outlook.com>
2022-05-27 18:58:13 -0300

    add Result to try reveived a request

--- src/server.rs
+++ src/server.rs
@@ -19,9 +19,14 @@ impl Server{
         loop{
             match listener.accept(){
                 Ok((mut stream, _)) => {
-                    let mut buf = [0; 1024];
-                    stream.read(&mut buf);
-                    println!("connected!")
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    }
                 }
                 Err(e) => println!("Failed to establish a connection: {}", e),
             }
